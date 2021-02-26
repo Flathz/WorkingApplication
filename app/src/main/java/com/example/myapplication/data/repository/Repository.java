7515891bc22ced7f5d1afb6d1baa45af8170bc08
Flath <Flath@DESKTOP-916916I>
2021-02-26 13:45:49 +0100

    repository rollback

--- app/src/main/java/com/example/myapplication/data/repository/Repository.java
+++ app/src/main/java/com/example/myapplication/data/repository/Repository.java
@@ -1,43 +1,9 @@
 package com.example.myapplication.data.repository;
 
-import androidx.lifecycle.LiveData;
-
-import com.example.myapplication.data.local.dao.NasaDao;
-import com.example.myapplication.data.local.entity.NasaEntity;
-import com.example.myapplication.data.remote.model.Nasa;
 import com.example.myapplication.data.remote.network.ApiService;
 import com.example.myapplication.data.remote.network.RetroInstance;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class Repository {
-
-    private NasaDao nasaDao = new NasaDao() {
-        @Override
-        public LiveData<List<NasaEntity>> getAllEntities() {
-            return nasaDao.getAllEntities();
-        }
-
-        @Override
-        public void insertAll(NasaEntity... nasaEntities) {
-            nasaDao.insertAll(nasaEntities);
-        }
-
-        @Override
-        public void insert(NasaEntity nasaEntity) {
-            nasaDao.insert(nasaEntity);
-
-        }
-
-        @Override
-        public void delete(NasaEntity nasaEntity) {
-            nasaDao.delete(nasaEntity);
-        }
-    };
 
     private ApiService apiService = RetroInstance.getRetrofit().create(ApiService.class);
 
@@ -50,8 +16,5 @@ public class Repository {
         return apiService;
     }
 
-    public NasaDao getNasaDao() {
-        return nasaDao;
-    }
 
 }
